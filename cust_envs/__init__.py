470c98f728c9127f34c706c7fd8f0ec5c6667f76
Ben Ling <belinghy@gmail.com>
2018-05-23 15:33:40 -0700

    model for crab

---
+++ cust_envs/__init__.py
@@ -0,0 +1,24 @@
+import gym
+from gym.envs.registration import registry, make, spec
+
+
+def register(id, *args, **kvargs):
+    if id in registry.env_specs:
+        return
+    else:
+        return gym.envs.registration.register(id, *args, **kvargs)
+
+
+register(
+    id='Walker2DCustomEnv-v0',
+    entry_point='cust_envs.envs:Walker2DCustomEnv',
+    max_episode_steps=1000,
+    reward_threshold=2500.0
+)
+
+register(
+    id='Crab2DCustomEnv-v0',
+    entry_point='cust_envs.envs:Crab2DCustomEnv',
+    max_episode_steps=1000,
+    reward_threshold=2500.0
+)
---
+++ enjoy_crab2d.py
@@ -0,0 +1,36 @@
+import gym
+import numpy as np
+import cust_envs
+import time
+
+def main():
+    env = gym.make("Crab2DCustomEnv-v0")
+    env.render(mode="human")
+
+    env.reset()
+
+    while 1:
+        frame = 0
+        score = 0
+        restart_delay = 0
+        obs = env.reset()
+
+        while 1:
+            time.sleep(1./60.)
+            obs, r, done, _ = env.step(np.zeros(6))
+            score += r
+            frame += 1
+
+            still_open = env.render("human")
+            if still_open==False:
+                return
+            # if not done: continue
+            # if restart_delay==0:
+            #     print("score=%0.2f in %i frames" % (score, frame))
+            #     restart_delay = 60*2  # 2 sec at 60 fps
+            # else:
+            #     restart_delay -= 1
+            #     if restart_delay==0: break
+
+if __name__=="__main__":
+    main()
