c5b89b966682169c990d2f76a89a0bbc1280467c
Ben Ling <belinghy@gmail.com>
2018-05-25 13:29:56 -0700

    move files around

--- playground/enjoy_walker2d.py
+++ playground/enjoy_walker2d.py
@@ -1,3 +1,9 @@
+import os
+
+current_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+parent_dir = os.path.dirname(current_dir)
+os.sys.path.append(parent_dir)
+
 import gym
 import numpy as np
 import cust_envs
